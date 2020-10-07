28490fa5756959ac9f4eb1a3905c8b78b536a6ac
waiscodes <b.wais@outlook.com>
2020-10-07 11:18:11 -0600

    updated spelling mistake

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <h2><?php bloginfo('title'); ?> Footer</h2>
 <p>
 &copy;
-<?php echo date('Y'); ?>
+<?php echo date('Y'); ?> 
 <a href="<?php echo site_url(); ?>"><?php bloginfo('title'); ?></a>
 All Rights Reserved.
 </p>
